2f5fe81662d02815bd64a5259a9ad2fe97e861da
b1329016 <berris0125@gmail.com>
2024-12-12 15:10:22 +0800

    Update main3.c

--- Ex4/main3.c
+++ Ex4/main3.c
@@ -5,10 +5,10 @@ int main() {
     int x, y;
     for( y = 1, x = 1; y <= 9; x++)
     {
-        printf("%d*%d=%d ",y,x,y*x);
+        printf("%d*%d=%d\t ",y,x,y*x);
         if( x == 9)
         {
-            x = 1;
+            x = 0;
             y++;
             printf("\n");
         }    
